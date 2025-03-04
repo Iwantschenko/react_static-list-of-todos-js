613f706c414503e66c48ffdbbb35021fa1b4c02f
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-03-04 17:41:02 +0200

    Add Helper

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import './App.scss';
 
-import todosFromServer from './api/todos.json';
-import usersFromServer from './api/users.json';
-import { TodoList } from './components/TodoList/TodoList';
-
-function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
-}
-
-export const todos = todosFromServer.map(todo => ({
-  ...todo,
-  user: getUserById(todo.userId),
-}));
+import { TodoList } from './components/TodoList';
+import { todos } from './components/Helper';
 
 export const App = () => (
   <div className="App">
